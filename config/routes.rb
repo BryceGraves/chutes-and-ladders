4f289f5bb1db24a93b912c0c500eab3e6927bb22
Bryce Graves <brycegraves97@gmail.com>
2019-06-26 19:17:39 -0700

    Project: add better routing tests not passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :players
-  resources :spaces
+  resources :games do
+    resources :players
+  end
+
+  root 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
