9c8f68c7a754f35617785b0379323d21d1997b32
Bryce Graves <brycegraves97@gmail.com>
2019-06-29 10:17:30 -0700

    Routes: root now redirects to games with status 302

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :players
   end
 
-  root 'games#index'
+  root to: redirect(path: '/games', status: 302)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
