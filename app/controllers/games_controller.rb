b15997f7ade4e4c84903fe9e5d1d07131fdffb80
Bryce Graves <brycegraves97@gmail.com>
2019-07-01 10:26:35 -0700

    GamesControllerSpec: moved the controllers around

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,14 @@
 class GamesController < ApplicationController
-  def new
-    @game = Game.new
-  end
-
   def index
     @games = Game.all
   end
 
   def show
     @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
   end
 
   def create
