2c913276602c7a22622fbd4a7e25bfec3c34e88d
Bryce Graves <brycegraves97@gmail.com>
2019-06-30 20:32:39 -0700

    Spec: moved system tests into category folders

--- spec/system/add_player_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "Adding Player to Game Flow", type: :system do
-  #FIXME: Change this to be better and match a better flow. Mainly be better
-  it "A user can add a player to an active game" do
-    
-  end
-end
