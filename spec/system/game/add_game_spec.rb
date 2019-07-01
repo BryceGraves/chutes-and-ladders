aa713ede116e08f3ec168b0200a6e0cfde9b78f7
Bryce Graves <brycegraves97@gmail.com>
2019-06-30 20:35:56 -0700

    AddGameSpec: made fake names more descriptive

---
+++ spec/system/game/add_game_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Create Game Flow", type: :system do
+  it "A user can create a game" do
+    visit new_game_path
+    fill_in "Name", with: "FAKE GAME"
+    click_on("Create Game")
+    expect(page).to have_content("FAKE NAME")
+  end
+
+  it "A user cannot create a game with a blank name" do
+    visit new_game_path
+    fill_in "Name", with: ""
+    click_on("Create Game")
+    expect(page).to have_content("can't be blank")
+  end
+end
