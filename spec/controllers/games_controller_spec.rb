73cf4c2672a24cca389d18a4ed8cff94d305fad5
Bryce Graves <brycegraves97@gmail.com>
2019-06-30 20:53:42 -0700

    GamesControllerSpec: add test for index controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "Games index page" do
+    it "Returns status code 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
 
+    it "Returns all existing games" do
+      expect(Game).to receive(:all)
+      get :index
+    end
+  end
 end
