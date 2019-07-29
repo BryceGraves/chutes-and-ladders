cefd7077cf99fb143fdd6b765d6210a4d45ef8ee
Bryce Graves <brycegraves97@gmail.com>
2019-07-29 01:47:42 -0700

    AddGameSpec: add before each to remove duplication
    Reference: #52

--- spec/system/game/add_game_spec.rb
+++ spec/system/game/add_game_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Create Game Flow', type: :system do
-  it 'A user can create a game' do
-    visit new_game_path
-    fill_in 'Name', with: 'FAKE GAME'
-    click_on('Create Game')
-    expect(page).to have_content('FAKE GAME')
-  end
+  context 'After navigating to create a game page' do
+    before(:each) do
+      visit new_game_path
+    end
 
-  it 'A user cannot create a game with a blank name' do
-    visit new_game_path
-    fill_in 'Name', with: ''
-    click_on('Create Game')
-    expect(page).to have_content("can't be blank")
+    it 'A user can create a game' do
+      fill_in 'Name', with: 'FAKE GAME'
+      click_on('Create Game')
+
+      expect(page).to have_content('FAKE GAME')
+    end
+
+    it 'A user cannot create a game with a blank name' do
+      fill_in 'Name', with: ''
+      click_on('Create Game')
+
+      expect(page).to have_content("can't be blank")
+    end
   end
 end
