2715f8a2d629cbcf4735f0df8393ef5a94f9ac8b
Bryce Graves <brycegraves97@gmail.com>
2019-08-03 04:14:20 -0700

    PlayerSpec: add failing tests for delete
    Reference: #43

--- spec/system/player_spec.rb
+++ spec/system/player_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 # TODO: add seed data to remove the need to build a game for each test.
 RSpec.describe 'Player Flows', type: :system do
-  let(:game) { create(:game) }
+  let(:game) { create(:game, :has_players) }
 
   context 'Create Player Flow' do
     before(:each) do
@@ -22,6 +22,20 @@ RSpec.describe 'Player Flows', type: :system do
       click_on('Create Player')
 
       expect(page).to have_content("can't be blank")
+    end
+  end
+
+  context 'Delete Player Flow' do
+    before(:each) do
+      visit game_path(game)
+    end
+
+    after(:each) do
+      expect(page).not_to have_content('FAKE PLAYER ONE')
+    end
+
+    it 'A user can delete a player from game' do
+      click_on('Remove FAKE PLAYER ONE')
     end
   end
 end
