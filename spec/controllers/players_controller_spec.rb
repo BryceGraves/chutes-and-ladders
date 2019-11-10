e86f2081206e5155935132fb698db96b86d84eff
Bryce Graves <brycegraves97@gmail.com>
2019-11-09 20:41:18 -0800

    Specs: removed controller testing specs in favor of system specs

--- spec/controllers/players_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-#TODO: add controller tests for all methods
-RSpec.describe PlayersController, type: :controller do
-
-end
--- spec/system/play_spec.rb
+++ spec/system/play_spec.rb
@@ -3,15 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Play Game Flows', type: :system do
   context 'Play Game Flow' do
     before(:each) do
-      create(:game, :has_players)
+      @game = create(:game, :has_players)
     end
 
-    it 'A user can navigate to a game' do
+    it 'A user can navigate to play path of a created game' do
       visit games_path
       click_on('View FAKE GAME')
       click_on('Play Game')
 
       expect(page).to have_tag('div', :with => { :class => 'game-board' })
+    end
+
+    it 'A user can play a turn' do
+      visit game_play_index_path(@game.id)
+
+      player_one_position = find_by_id('scoreboard-table').first('tr').find('td', :text =>/Position: \d+/)
+      _, player_starting_position_string = player_one_position.text.split(' ')
+      
+      click_on('Take Turn')
+
+      player_one_position = find_by_id('scoreboard-table').first('tr').find('td', :text =>/Position: \d+/)
+      _, player_new_position_string = player_one_position.text.split(' ')
+
+      expect(player_new_position_string).not_to eq(player_starting_position_string)
     end
   end
 end
